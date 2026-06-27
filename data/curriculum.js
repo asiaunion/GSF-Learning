3509d317f2586bf01b1d5207b4f497221ffe2e82
GSF <gsf@GSFui-MacBookAir.local>
2026-06-27 19:17:02 +0900

    fix: migrate to ES module architecture and commit missing data files

---
+++ data/curriculum.js
@@ -0,0 +1,9 @@
+import hiragana from './phases/hiragana.js';
+import katakana from './phases/katakana.js';
+
+const CURRICULUM = {
+  appTitle: "にほんご フラッシュカード",
+  phases: [hiragana, katakana]
+};
+
+export default CURRICULUM;
---
+++ data/phases/katakana.js
@@ -0,0 +1,649 @@
+export default {
+  "id": "katakana",
+  "title": "가타카나",
+  "type": "katakana",
+  "lessons": [
+    {
+      "id": 15,
+      "title": "Lesson 15",
+      "row": "ア行",
+      "cards": [
+        {
+          "character": "ア",
+          "word": "アイス",
+          "wordReading": "アイス",
+          "meaningKo": "아이스크림",
+          "image": "images/realistic/aisu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "イ",
+          "word": "インク",
+          "wordReading": "インク",
+          "meaningKo": "잉크",
+          "image": "images/realistic/inku_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ウ",
+          "word": "ウクレレ",
+          "wordReading": "ウクレレ",
+          "meaningKo": "우쿨렐레",
+          "image": "images/realistic/ukurere_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "エ",
+          "word": "エプロン",
+          "wordReading": "エプロン",
+          "meaningKo": "앞치마",
+          "image": "images/realistic/epuron_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "オ",
+          "word": "オレンジ",
+          "wordReading": "オレンジ",
+          "meaningKo": "오렌지",
+          "image": "images/realistic/orenji_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 16,
+      "title": "Lesson 16",
+      "row": "カ行",
+      "cards": [
+        {
+          "character": "カ",
+          "word": "カメラ",
+          "wordReading": "カメラ",
+          "meaningKo": "카메라",
+          "image": "images/realistic/kamera_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "キ",
+          "word": "キウイ",
+          "wordReading": "キウイ",
+          "meaningKo": "키위",
+          "image": "images/realistic/kiui_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ク",
+          "word": "クッキー",
+          "wordReading": "クッキー",
+          "meaningKo": "쿠키",
+          "image": "images/realistic/kukkii_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ケ",
+          "word": "ケーキ",
+          "wordReading": "ケーキ",
+          "meaningKo": "케이크",
+          "image": "images/realistic/keeki_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "コ",
+          "word": "コップ",
+          "wordReading": "コップ",
+          "meaningKo": "컵",
+          "image": "images/realistic/koppu_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 17,
+      "title": "Lesson 17",
+      "row": "サ行",
+      "cards": [
+        {
+          "character": "サ",
+          "word": "サラダ",
+          "wordReading": "サラダ",
+          "meaningKo": "샐러드",
+          "image": "images/realistic/sarada_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "シ",
+          "word": "シャツ",
+          "wordReading": "シャツ",
+          "meaningKo": "셔츠",
+          "image": "images/realistic/shatsu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ス",
+          "word": "スプーン",
+          "wordReading": "スプーン",
+          "meaningKo": "스푼",
+          "image": "images/realistic/supuun_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "セ",
+          "word": "セーター",
+          "wordReading": "セーター",
+          "meaningKo": "스웨터",
+          "image": "images/realistic/seetaa_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ソ",
+          "word": "ソーセージ",
+          "wordReading": "ソーセージ",
+          "meaningKo": "소시지",
+          "image": "images/realistic/sooseeji_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 18,
+      "title": "Lesson 18",
+      "row": "タ行",
+      "cards": [
+        {
+          "character": "タ",
+          "word": "タオル",
+          "wordReading": "タオル",
+          "meaningKo": "수건",
+          "image": "images/realistic/taoru_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "チ",
+          "word": "チーズ",
+          "wordReading": "チーズ",
+          "meaningKo": "치즈",
+          "image": "images/realistic/chiizu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ツ",
+          "word": "ツリー",
+          "wordReading": "ツリー",
+          "meaningKo": "트리",
+          "image": "images/realistic/tsurii_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "テ",
+          "word": "テント",
+          "wordReading": "テント",
+          "meaningKo": "텐트",
+          "image": "images/realistic/tento_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ト",
+          "word": "トマト",
+          "wordReading": "トマト",
+          "meaningKo": "토마토",
+          "image": "images/realistic/tomato_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 19,
+      "title": "Lesson 19",
+      "row": "ナ行",
+      "cards": [
+        {
+          "character": "ナ",
+          "word": "ナイフ",
+          "wordReading": "ナイフ",
+          "meaningKo": "나이프",
+          "image": "images/realistic/naifu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ニ",
+          "word": "ニット",
+          "wordReading": "ニット",
+          "meaningKo": "니트",
+          "image": "images/realistic/nitto_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ヌ",
+          "word": "ヌードル",
+          "wordReading": "ヌードル",
+          "meaningKo": "국수",
+          "image": "images/realistic/nuudoru_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ネ",
+          "word": "ネクタイ",
+          "wordReading": "ネクタイ",
+          "meaningKo": "넥타이",
+          "image": "images/realistic/nekutai_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ノ",
+          "word": "ノート",
+          "wordReading": "ノート",
+          "meaningKo": "공책",
+          "image": "images/realistic/nooto_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 20,
+      "title": "Lesson 20",
+      "row": "ハ行",
+      "cards": [
+        {
+          "character": "ハ",
+          "word": "ハム",
+          "wordReading": "ハム",
+          "meaningKo": "햄",
+          "image": "images/realistic/hamu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ヒ",
+          "word": "ヒーター",
+          "wordReading": "ヒーター",
+          "meaningKo": "히터",
+          "image": "images/realistic/hiitaa_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "フ",
+          "word": "フォーク",
+          "wordReading": "フォーク",
+          "meaningKo": "포크",
+          "image": "images/realistic/fooku_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ヘ",
+          "word": "ヘリコプター",
+          "wordReading": "ヘリコプター",
+          "meaningKo": "헬리콥터",
+          "image": "images/realistic/herikoputaa_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ホ",
+          "word": "ホース",
+          "wordReading": "ホース",
+          "meaningKo": "호스",
+          "image": "images/realistic/hoosu_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 21,
+      "title": "Lesson 21",
+      "row": "マ行",
+      "cards": [
+        {
+          "character": "マ",
+          "word": "マイク",
+          "wordReading": "マイク",
+          "meaningKo": "마이크",
+          "image": "images/realistic/maiku_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ミ",
+          "word": "ミルク",
+          "wordReading": "ミルク",
+          "meaningKo": "우유",
+          "image": "images/realistic/miruku_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ム",
+          "word": "ムービー",
+          "wordReading": "ムービー",
+          "meaningKo": "영화",
+          "image": "images/realistic/muubii_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "メ",
+          "word": "メロン",
+          "wordReading": "メロン",
+          "meaningKo": "멜론",
+          "image": "images/realistic/meron_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "モ",
+          "word": "モニター",
+          "wordReading": "モニター",
+          "meaningKo": "모니터",
+          "image": "images/realistic/monitaa_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 22,
+      "title": "Lesson 22",
+      "row": "ラ行",
+      "cards": [
+        {
+          "character": "ラ",
+          "word": "ライオン",
+          "wordReading": "ライオン",
+          "meaningKo": "사자",
+          "image": "images/realistic/raion_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "リ",
+          "word": "リボン",
+          "wordReading": "リボン",
+          "meaningKo": "리본",
+          "image": "images/realistic/ribon_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ル",
+          "word": "ルビー",
+          "wordReading": "ルビー",
+          "meaningKo": "루비",
+          "image": "images/realistic/rubii_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "レ",
+          "word": "レモン",
+          "wordReading": "レモン",
+          "meaningKo": "레몬",
+          "image": "images/realistic/remon_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ロ",
+          "word": "ロボット",
+          "wordReading": "ロボット",
+          "meaningKo": "로봇",
+          "image": "images/realistic/robotto_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 23,
+      "title": "Lesson 23",
+      "row": "ヤ・ワ行",
+      "cards": [
+        {
+          "character": "ヤ",
+          "word": "タイヤ",
+          "wordReading": "タイヤ",
+          "meaningKo": "타이어",
+          "image": "images/realistic/taiya_realistic.webp",
+          "highlightIndex": 2
+        },
+        {
+          "character": "ユ",
+          "word": "ユニフォーム",
+          "wordReading": "ユニフォーム",
+          "meaningKo": "유니폼",
+          "image": "images/realistic/yunifoomu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ヨ",
+          "word": "ヨーヨー",
+          "wordReading": "ヨーヨー",
+          "meaningKo": "요요",
+          "image": "images/realistic/yooyoo_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ワ",
+          "word": "ワッフル",
+          "wordReading": "ワッフル",
+          "meaningKo": "와플",
+          "image": "images/realistic/waffuru_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ン",
+          "word": "パン",
+          "wordReading": "パン",
+          "meaningKo": "빵",
+          "image": "images/realistic/pan_realistic.webp",
+          "highlightIndex": 1
+        }
+      ]
+    },
+    {
+      "id": 24,
+      "title": "Lesson 24",
+      "row": "ガ行 (탁음)",
+      "cards": [
+        {
+          "character": "ガ",
+          "word": "ガラス",
+          "wordReading": "ガラス",
+          "meaningKo": "유리",
+          "image": "images/realistic/garasu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ギ",
+          "word": "ギター",
+          "wordReading": "ギター",
+          "meaningKo": "기타",
+          "image": "images/realistic/gitaa_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "グ",
+          "word": "グラス",
+          "wordReading": "グラス",
+          "meaningKo": "유리잔",
+          "image": "images/realistic/gurasu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ゲ",
+          "word": "ゲーム",
+          "wordReading": "ゲーム",
+          "meaningKo": "게임",
+          "image": "images/realistic/geemu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ゴ",
+          "word": "ゴリラ",
+          "wordReading": "ゴリラ",
+          "meaningKo": "고릴라",
+          "image": "images/realistic/gorira_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 25,
+      "title": "Lesson 25",
+      "row": "ザ行 (탁음)",
+      "cards": [
+        {
+          "character": "ザ",
+          "word": "ピザ",
+          "wordReading": "ピザ",
+          "meaningKo": "피자",
+          "image": "images/realistic/piza_realistic.webp",
+          "highlightIndex": 1
+        },
+        {
+          "character": "ジ",
+          "word": "ラジオ",
+          "wordReading": "ラジオ",
+          "meaningKo": "라디오",
+          "image": "images/realistic/rajio_realistic.webp",
+          "highlightIndex": 1
+        },
+        {
+          "character": "ズ",
+          "word": "ズボン",
+          "wordReading": "ズボン",
+          "meaningKo": "바지",
+          "image": "images/realistic/zubon_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ゼ",
+          "word": "ゼリー",
+          "wordReading": "ゼリー",
+          "meaningKo": "젤리",
+          "image": "images/realistic/zerii_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ゾ",
+          "word": "パズル",
+          "wordReading": "パズル",
+          "meaningKo": "퍼즐",
+          "image": "images/realistic/pazuru_realistic.webp",
+          "highlightIndex": 1
+        }
+      ]
+    },
+    {
+      "id": 26,
+      "title": "Lesson 26",
+      "row": "ダ行 (탁음)",
+      "cards": [
+        {
+          "character": "ダ",
+          "word": "ダンス",
+          "wordReading": "ダンス",
+          "meaningKo": "춤",
+          "image": "images/realistic/dansu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "デ",
+          "word": "デスク",
+          "wordReading": "デスク",
+          "meaningKo": "책상",
+          "image": "images/realistic/desuku_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ド",
+          "word": "ドア",
+          "wordReading": "ドア",
+          "meaningKo": "문",
+          "image": "images/realistic/doa_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 27,
+      "title": "Lesson 27",
+      "row": "バ行 (탁음)",
+      "cards": [
+        {
+          "character": "バ",
+          "word": "バス",
+          "wordReading": "バス",
+          "meaningKo": "버스",
+          "image": "images/realistic/basu_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ビ",
+          "word": "ビル",
+          "wordReading": "ビル",
+          "meaningKo": "빌딩",
+          "image": "images/realistic/biru_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ブ",
+          "word": "ブロック",
+          "wordReading": "ブロック",
+          "meaningKo": "블록",
+          "image": "images/realistic/burokku_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ベ",
+          "word": "ベッド",
+          "wordReading": "ベッド",
+          "meaningKo": "침대",
+          "image": "images/realistic/beddo_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ボ",
+          "word": "ボタン",
+          "wordReading": "ボタン",
+          "meaningKo": "버튼",
+          "image": "images/realistic/botan_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    },
+    {
+      "id": 28,
+      "title": "Lesson 28",
+      "row": "パ行 (반탁음)",
+      "cards": [
+        {
+          "character": "パ",
+          "word": "パソコン",
+          "wordReading": "パソコン",
+          "meaningKo": "컴퓨터",
+          "image": "images/realistic/pasokon_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ピ",
+          "word": "ピアノ",
+          "wordReading": "ピアノ",
+          "meaningKo": "피아노",
+          "image": "images/realistic/piano_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "プ",
+          "word": "プール",
+          "wordReading": "プール",
+          "meaningKo": "수영장",
+          "image": "images/realistic/puuru_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ペ",
+          "word": "ペンギン",
+          "wordReading": "ペンギン",
+          "meaningKo": "펭귄",
+          "image": "images/realistic/pengin_realistic.webp",
+          "highlightIndex": 0
+        },
+        {
+          "character": "ポ",
+          "word": "ポスト",
+          "wordReading": "ポスト",
+          "meaningKo": "우체통",
+          "image": "images/realistic/posuto_realistic.webp",
+          "highlightIndex": 0
+        }
+      ]
+    }
+  ]
+};
